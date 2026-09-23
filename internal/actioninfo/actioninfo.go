36f00fa31454b7a567a225d2741b31ede2288769
Александр Коробков <alexmutko1488@yandex.ru>
2026-09-23 15:55:15 +0300

    first commit

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,32 @@
 package actioninfo
 
+import (
+	"fmt"
+	"log"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(datastring string) error
+	ActionInfo() (string, error)
 }
 
-func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+func Info(dataStrings []string, parsers []DataParser) ([]string, error) {
+	if len(dataStrings) != len(parsers) {
+		return nil, fmt.Errorf("количество строк данных (%d) не совпадает с количеством парсеров (%d)", len(dataStrings), len(parsers))
+	}
+	var results []string
+	for i := 0; i < len(dataStrings); i++ {
+		err := parsers[i].Parse(dataStrings[i])
+		if err != nil {
+			log.Printf("Ошибка парсинга строки")
+			continue
+		}
+		report, err := parsers[i].ActionInfo()
+		if err != nil {
+			log.Printf("Ошибка формирования отчёта")
+			continue
+		}
+		results = append(results, report)
+	}
+	return results, nil
 }
--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -2,6 +2,7 @@ package spentenergy
 
 import (
 	"time"
+	"fmt"
 )
 
 // Основные константы, необходимые для расчетов.
@@ -14,16 +15,43 @@ const (
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("все данные должны быть больше 0")
+	}
+	meanSpeed := MeanSpeed(steps, height, duration)
+	durationInMinutes := duration.Minutes()
+	walkingCalories := (weight * meanSpeed * durationInMinutes) / minInH * walkingCaloriesCoefficient
+	return walkingCalories, nil
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
 	// TODO: реализовать функцию
+	if steps <= 0 || weight <= 0 || height <= 0 || duration <= 0 {
+		return 0, fmt.Errorf("все данные должны быть больше 0")
+	}
+	meanSpeed := MeanSpeed(steps, height, duration)
+	durationInMinutes := duration.Minutes()
+	runningCalories := (weight * meanSpeed * durationInMinutes) / minInH
+	return runningCalories, nil
 }
 
 func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 	// TODO: реализовать функцию
+	if duration <= 0 {
+		return 0
+	}
+	 if steps < 0 {
+        return 0
+    }
+	distance := Distance(steps, height)
+	durationInHours := duration.Hours()
+	meanSpeed := distance / durationInHours
+	return meanSpeed
 }
 
 func Distance(steps int, height float64) float64 {
 	// TODO: реализовать функцию
+	stepLength := height * stepLengthCoefficient
+	distance := (float64(steps) * stepLength) / mInKm
+	return distance
 }
